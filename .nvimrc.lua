eeb9fc572323bb1ffc888831589b34e44b17a0d3
Mike Mehl <michaelalanmehl@gmail.com>
2023-12-08 18:35:23 -0500

    Remove nvimrc options that are unused.

--- .nvimrc.lua
+++ .nvimrc.lua
@@ -1,28 +1,28 @@
-local dap = require("dap")
-dap.adapters.delve = {
-	type = "server",
-	host = "127.0.0.1",
-	port = "34567",
-	executable = {
-		command = "dlv",
-		args = {
-			"dap",
-			"--listen=127.0.0.1:34567",
-			"--log",
-			"--log-output=dap",
-		},
-	},
-}
-
-local overseer = require("overseer")
-overseer
-	.new_task({
-		name = "build",
-		cmd = "just build",
-		components = {
-			{ "restart_on_save" },
-			{ "unique", { replace = true } },
-			"default",
-		},
-	})
-	:start()
+-- local dap = require("dap")
+-- dap.adapters.delve = {
+-- 	type = "server",
+-- 	host = "127.0.0.1",
+-- 	port = "34567",
+-- 	executable = {
+-- 		command = "dlv",
+-- 		args = {
+-- 			"dap",
+-- 			"--listen=127.0.0.1:34567",
+-- 			"--log",
+-- 			"--log-output=dap",
+-- 		},
+-- 	},
+-- }
+--
+-- local overseer = require("overseer")
+-- overseer
+-- 	.new_task({
+-- 		name = "build",
+-- 		cmd = "just build",
+-- 		components = {
+-- 			{ "restart_on_save" },
+-- 			{ "unique", { replace = true } },
+-- 			"default",
+-- 		},
+-- 	})
+-- 	:start()
